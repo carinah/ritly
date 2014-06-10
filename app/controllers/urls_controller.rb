bc77d4a109b826a6fe2a994ab493ce2525b62962
carinah <carina@generalassemb.ly>
2014-06-10 12:57:46 -0700

    added edit and update

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,12 +4,40 @@ class UrlsController < ApplicationController
   end 
 
   def create
-    url = params[:url]
-    Url.create(url)
-    # random_string = SecureRandom.urlsafe_base64(length_of_string).8 
-    redirect_to "/urls"
+    url = params[:url].permit(:link, :random_string)
+
+    new_url = Url.create(url)
+    new_url.random_string = SecureRandom.urlsafe_base64(5, true)
+    new_url.save
+    redirect_to url_path(new_url)
+  end 
+
+  def index 
+    urls = Url.all 
   end 
 
   def show 
+    url = params[:id]
+    @new_url = Url.find(url)
   end 
+
+  def edit 
+    url_id = params[:id]
+    @url = Url.find(url_id)
+  end 
+
+  def update 
+    url_id = params[:id]
+    @url = Url.find(url_id) 
+
+    updated_url = params.require[:url].permit[:link, :random_string] 
+    Url.updated_attribtues(updated_url)
+
+    redirect_to url_path(url_id)  
+  end 
+
+  def index 
+    #urls = Url.all 
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root to: "urls#new"
 
-  get "/urls", to: "urls#new", as: "url" 
+  get "/urls", to: "urls#new", as: "urls" 
 
   post "/urls", to: "urls#create"
 
-  get "/urls/show", to: "urls#show"
+  get "/urls/:id", to: "urls#show", as: "url"
+
+  get "/urls/:id/edit", to: "urls#edit" 
+
+  put "/urls/:id", to: "urls#update"
+
+  get "/urls/all", to: "urls#index", as: "index"  
 end 
 
 # Rails convention: Model is always singular, table is always plural 
