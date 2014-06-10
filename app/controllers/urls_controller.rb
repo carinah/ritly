890fdeda790c1e2280c6ae251943cc2fd5738d9b
carinah <carina@generalassemb.ly>
2014-06-10 11:25:20 -0700

    created show page and create method

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,6 +4,12 @@ class UrlsController < ApplicationController
   end 
 
   def create
+    url = params[:url]
+    Url.create(url)
+    # random_string = SecureRandom.urlsafe_base64(length_of_string).8 
     redirect_to "/urls"
+  end 
+
+  def show 
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "urls#new"
 
-  get "/urls", to: "urls#new"
+  get "/urls", to: "urls#new", as: "url" 
+
+  post "/urls", to: "urls#create"
+
+  get "/urls/show", to: "urls#show"
 end 
 
 # Rails convention: Model is always singular, table is always plural 
